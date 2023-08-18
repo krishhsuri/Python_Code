3bf9c6c76d11d02397abe22f4635877c4b9b01d2
krish <krish050805@gmail.com>
2023-08-18 21:16:46 +0530

    new

---
+++ Pattern_printing/ptrn_6.py
@@ -0,0 +1,12 @@
+ #     *
+ #    * *
+ #   * * *
+ #  * * * *
+ # * * * * *
+x = int(input(":"))
+for i in range(x):
+    for j in range(i,x):
+        print("",end=" ")
+    for j in range(i+1):
+        print("*",end= " ")
+    print()
